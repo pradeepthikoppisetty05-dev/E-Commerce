5eee18468a93823ec0ba0cb583f0cf5647a41633
Deepthi <pradeepthikoppisetty05@gmail.com>
2026-02-05 12:54:58 +0530

    Initial setup

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/profile', [ProfileController::class, 'profile']);
+    Route::put('/profile', [ProfileController::class, 'update']);
+
+    Route::put('/change-password', [ProfileController::class, 'change']);
+});
