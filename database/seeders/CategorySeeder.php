b95e4422c4cf361f1ace58c4128da853c9a73a5a
Deepthi <pradeepthikoppisetty05@gmail.com>
2026-02-05 14:50:43 +0530

    Added seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::insert([
+        ['name' => 'Electronics'],
+        ['name' => 'Fashion'],
+        ['name' => 'Books'],
+        ['name' => 'Home Appliances'],
+        ]);
+    }
+}
