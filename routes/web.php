eb1bab0cf4aca669fc54419fa898cdb7ce1b7983
SaiPranith18 <saipranith235@gmail.com>
2026-02-05 15:20:25 +0530

    Refactor routes to use controllers and resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/car/edit', [CarController::class, 'edit'])->name('car.edit');
+
+
+Route::resource('car', CarController::class);
+
+Route::get('/signup', [SignupController::class, 'create'])->name('signup');
+// Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::match(['get','post'], '/login', [LoginController::class, 'create'])->name('login');
+
