6adbd7cff8356d0b1267582b3815ee12fae9891a
Deepthi <pradeepthikoppisetty05@gmail.com>
2026-02-05 16:33:11 +0530

    category API

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/card/edit', [CardController::class, 'edit'])->name('card.edit');
-
-
-Route::resource('card', CardController::class);
 
 Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 // Route::get('/login', [LoginController::class, 'create'])->name('login');
