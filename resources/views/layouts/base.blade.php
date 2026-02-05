6bf26e3cfde7c93c50faa6a80061bbc30783dc13
SaiPranith18 <saipranith235@gmail.com>
2026-02-05 18:03:53 +0530

    Change page title from 'Bike Nature' to 'E-Commerce'

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <meta name="crsf-token" content="{{ csrf_token() }}">
-    <title>{{ $title }} | {{ 'Bike Nature' }}</title>
+    <title>{{ $title }} | {{ 'E-Commerce' }}</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
